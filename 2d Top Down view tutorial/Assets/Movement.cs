a3c2e1d232f1882533fe1d10e392263c7509332c
choijongha <80575094+choijongha@users.noreply.github.com>
2022-02-18 20:35:11 +0900

    8방향 움직임, 애니메이션 완료

--- 2d Top Down view tutorial/Assets/Movement.cs
+++ 2d Top Down view tutorial/Assets/Movement.cs
@@ -14,9 +14,25 @@ public class Movement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        playerRb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * playerMoveSpeed * Time.deltaTime;
-
+        // Rigidbody2D.velocity
+        /*playerRb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) ;
         myAnim.SetFloat("MoveX", playerRb.velocity.x);
         myAnim.SetFloat("MoveY", playerRb.velocity.y);
+        */
+
+        // Transform.position
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        Vector3 moveVector = new Vector2(moveX, moveY);
+        playerRb.transform.position += moveVector.normalized * playerMoveSpeed *Time.deltaTime;
+        myAnim.SetFloat("MoveX", moveX);
+        myAnim.SetFloat("MoveY", moveY);
+        
+        if(Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
+        {
+            myAnim.SetFloat("LastMoveX", Input.GetAxisRaw("Horizontal"));
+            myAnim.SetFloat("LastMoveY", Input.GetAxisRaw("Vertical"));
+        }
+        
     }
 }
