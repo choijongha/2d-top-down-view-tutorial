21c82ad537df8b67e85e3d0b423c84403ce85aac
choijongha <80575094+choijongha@users.noreply.github.com>
2022-02-25 20:39:18 +0900

    공격, 체력 작업 기초
    하는 중

--- 2d Top Down view tutorial/Assets/EnemyController.cs
+++ 2d Top Down view tutorial/Assets/EnemyController.cs
@@ -1,16 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    private Animator anim;
     [SerializeField] Transform target;
+    // 따라가는 움직임 속도.
     [SerializeField] float speed = 3f;
+    // 돌아가는 움직임 속도.
     [SerializeField] float backSpeed = 10f;
-    [SerializeField] float attackMaxRange;
-    [SerializeField] float attackMinRange;
+    // 공격하기 위해 따라가는 범위
+    [SerializeField] float followRange = 5f;
+    // 따라감을 멈추고 공격하는 범위 안.
+    [SerializeField] float attackRange = 1.3f;  
+    // 적이 되돌아가는 위치.
     [SerializeField] Transform homePos;
+    private Animator anim;
     private float initialSpeed;
     private void Awake()
     {
@@ -28,14 +31,14 @@ public class EnemyController : MonoBehaviour
 
     private void Movement()
     {
-        if (Vector3.Distance(target.position, transform.position) <= attackMaxRange && Vector3.Distance(target.position, transform.position) >= attackMinRange)
+        if (Vector3.Distance(target.position, transform.position) <= followRange && Vector3.Distance(target.position, transform.position) >= attackRange)
         {
             speed = initialSpeed;
             transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
             anim.SetFloat("MoveX", target.position.x - transform.position.x);
             anim.SetBool("IsMoving", true);
         }
-        else if (Vector3.Distance(target.position, transform.position) < attackMinRange)
+        else if (Vector3.Distance(target.position, transform.position) < attackRange)
         {
             anim.SetBool("IsMoving", false);
             anim.SetTrigger("Attack");
@@ -43,8 +46,7 @@ public class EnemyController : MonoBehaviour
         else
         {
             backHome();
-        }           
-        
+        }                  
     }
     private void backHome()
     {
@@ -58,5 +60,4 @@ public class EnemyController : MonoBehaviour
             anim.SetBool("IsMoving", false);
         }
     }
-
 }
